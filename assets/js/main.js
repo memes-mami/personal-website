3cfce2c6a61dd1ddf1fc773d3ea3389d331e09e0
Vishnu_Teja <93179274+memes-mami@users.noreply.github.com>
2025-07-10 15:45:53 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,9 @@
    * Header toggle for mobile nav
    */
   const headerToggleBtn = document.querySelector('.header-toggle');
+document.querySelector('.header-toggle')?.addEventListener('click', function () {
+  document.querySelector('.header')?.classList.toggle('header-show');
+});
 
   function headerToggle() {
     document.querySelector('#header').classList.toggle('header-show');
